f6f2acc14027a9ba49e5c2f3c03202632cf00e50
chentao8818 <120267496+chentao8818@users.noreply.github.com>
2023-01-02 18:07:15 +0800

    Update theme_common.js

--- js/theme_common.js
+++ js/theme_common.js
@@ -1,66 +1,6 @@
         var audio_music=document.getElementById('audio_music'); 
         var audio_record=document.getElementById('audio_record'); 
         
-        if(typeof(music_json['music_select'])!='undefined' && music_json['music_select']!='null' && music_json['music_select']!=''){
-            if(music_json['music_select']=='m_online' && music_json['m_online_url']!='null' && music_json['m_online_url']!=''){ //选择在线列表
-                $('#audio_music').attr('src',music_json['m_online_url']);
-            }
-            if(music_json['music_select']=='m_upload' && music_json['m_upload_url']!='null' && music_json['m_upload_url']!=''){ //选择在线列表并且上传了歌曲
-                $('#audio_music').attr('src',music_json['m_upload_url']);
-            }
-            if(music_json['music_select']=='m_upload' && (music_json['m_upload_url']=='null' || music_json['m_upload_url']=='')){ //选择在线列表但是没有上传歌曲
-                console.log('music_select m_upload but m_upload_url is null, set defaulted music');
-                var random_music=random_music_as();
-                $('#audio_music').attr('src',random_music);
-            }
-            if(theme!='audio_list' || (theme=='audio_list' && start_id!='null')){
-                audio_music.play(); //触发音乐自动播放
-            }else{
-                audio_music.pause();
-                console.log('audio_list && no start');
-            }
-        }else{ //全新作品或空作品
-            console.log('set random music');
-            var random_music=random_music_as();
-            $('#audio_music').attr('src',random_music);
-            if(theme!='audio_list' || (theme=='audio_list' && start_id!='null')){
-                audio_music.play(); //触发音乐自动播放
-            }else{
-                audio_music.pause();
-                console.log('audio_list && no start');
-            }
-        }
-
-        if(typeof(record_json['record_bool'])!='undefined' && record_json['record_bool']!='null' && record_json['record_bool']!=''){
-            if(record_json['record_bool']=='r_true' && record_json['r_wechat_url']!='null' && record_json['r_wechat_url']!=''){ //选择要语音
-                $('#audio_record').attr('src',record_json['r_wechat_url']);
-            }
-            if(record_json['record_bool']=='r_true' && (record_json['r_wechat_url']=='null' || record_json['r_wechat_url']=='')){ //选择要语音，但却没有录语音
-                $('#div_record').hide(); //不显示
-                $('#div_record_tips').hide();
-            }
-            if(record_json['record_bool']=='r_false'){ //如果不要语音则不显示
-                $('#div_record').hide();
-                $('#div_record_tips').hide();
-            }
-        }else{
-            if(theme_content['bool_save']==false){ //全新作品或未保存内容，且未定义语音
-                console.log('set random record');
-                $('#audio_record').attr('src','http://cdn.aitetu520.com/chongqin_shenlin.mp3');
-            }else{ //非全新作品或已保存内容，但未定义语音，则不显示语音
-                $('#div_record').hide();
-                $('#div_record_tips').hide();
-            }
-        }
-
-
-
-        function random_music_as(){  //获取随机的模板图片
-            // console.log('random_words_as'); 
-            var random_num=Math.floor(Math.random()*(array_as_music.length)); //随机取值 
-            var random_music=array_as_music[random_num];
-            return random_music;
-        }
 
         //控制音乐切换播放暂停 
         var img_music=document.getElementById('img_music');
